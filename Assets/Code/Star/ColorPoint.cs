6ff847badcbe79ce4f72b9b87cf83b443b077a3c
Дон <dsvinkabot@gmail.com>
2022-12-16 02:42:21 +0300

    Star

---
+++ Assets/Code/Star/ColorPoint.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+namespace Code.Star
+{
+    [Serializable]
+    public class ColorPoint
+    {
+        public Vector3 Position;
+        public Color Color;
+    }
+}
---
+++ Assets/Code/Star/ColorPointTester.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Code.Star
+{
+    public class ColorPointTester: MonoBehaviour
+    {
+        public ColorPoint Point;
+        public ColorPoint[] Points;
+    }
+}
